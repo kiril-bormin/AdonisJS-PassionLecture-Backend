e0c00265718fe4825a6ac601843da75a8987ab51
Kiril Bormin <borminnkiril@gmail.com>
2026-05-12 15:52:54 +0200

    chore(livre) : ajout de commantaires dans le book controller

--- backend/app/controllers/books_controller.ts
+++ backend/app/controllers/books_controller.ts
@@ -3,22 +3,25 @@ import Book from '#models/book'
 
 export default class BooksController {
   async index({ request }: HttpContext) {
-    const q = request.input('q')
+    // récupèrer les filtres de recherche
+    const q = request.input('q') //
     const categoryId = request.input('categoryId')
     const authorId = request.input('authorId')
     const publisherId = request.input('publisherId')
     const page = request.input('page')
     const limit = request.input('limit', 20)
 
-    const query = Book.query()
+    const query = Book.query() // préparation d'une rêquete avec des champs préchargés
       .preload('author')
       .preload('category')
       .preload('publisher')
       .preload('user')
 
     if (q) {
+      // si q n'est pas null ou indifined
       query.where((builder) => {
-        builder.whereILike('title', `%${q}%`).orWhereILike('summary', `%${q}%`)
+        // execute le filtre
+        builder.whereILike('title', `%${q}%`).orWhereILike('summary', `%${q}%`) // whereILike ignore la case, % -> pour les caractères jokers
       })
     }
 
@@ -35,13 +38,14 @@ export default class BooksController {
     }
 
     if (page) {
-      return query.paginate(page, limit)
+      return query.paginate(page, limit) // .paginate calcul automatiquement les sauts en fonction de limit
     }
 
     return query
   }
 
   async show({ params }: HttpContext) {
+    // détails d'un livre spécifique
     return Book.query()
       .where('id', params.id)
       .preload('author')
@@ -49,17 +53,19 @@ export default class BooksController {
       .preload('publisher')
       .preload('user')
       .preload('reviews')
-      .firstOrFail()
+      .firstOrFail() // récupère le premier id (vue qu'il est unique)l
   }
 
   async store({ auth, request, response }: HttpContext) {
-    const user = await auth.authenticate()
+    const user = await auth.authenticate() // si l'utilisateur est mal authentifié(token invalide, expiré ou absent), la méthode s'arrête
 
-    const pagesCount = request.input('pagesCount', request.input('pages'))
+    const pagesCount = request.input('pagesCount', request.input('pages')) // serveur cherche "pagesCount" dans le body de la requête envoyée par l'utilisateur, si ce champ n'est pas présent,
+    // il essaie de trouve un champ nommé "pages". C'est fait afin de s'assurer qu'on recevra le nombre de page peu importe comment le champ s'appelle.
     const editionYear = request.input('editionYear', request.input('year'))
     const extractUrl = request.input('extractUrl', request.input('excerptUrl'))
 
     const payload = {
+      // dans payload on récupère les données du body (ainsi que les 3 variables qu'on a crée plus haut)
       title: request.input('title'),
       summary: request.input('summary'),
       pagesCount,
@@ -72,16 +78,17 @@ export default class BooksController {
       userId: user.id,
     }
 
-    const book = await Book.create(payload)
-    response.created(book)
+    const book = await Book.create(payload) // création du livre dans la db
+    response.created(book) // envoyer la réponse 201 (Created) avec l'objet créé
   }
 
   async update({ auth, params, request, response }: HttpContext) {
-    const user = await auth.authenticate()
-    const book = await Book.findOrFail(params.id)
+    const user = await auth.authenticate() // vérifier si l'utilisateur est authentifié
+    const book = await Book.findOrFail(params.id) // on récupère le livre par son id
 
     if (book.userId !== user.id && user.role !== 'admin') {
-      return response.forbidden({ message: 'You do not have permission to edit this book.' })
+      // vérification si l'utilisateur est propriétaire du livre ou admin
+      return response.forbidden({ message: 'You do not have permission to edit this book.' }) // retourne l'erreur 403 forbidden (manque de permissions)
     }
 
     const pagesCount = request.input('pagesCount', request.input('pages'))
@@ -89,7 +96,8 @@ export default class BooksController {
     const extractUrl = request.input('extractUrl', request.input('excerptUrl'))
 
     book.merge({
-      title: request.input('title', book.title),
+      // merger les changements
+      title: request.input('title', book.title), // deuxième valeur est la valeur par défaut
       summary: request.input('summary', book.summary),
       pagesCount: pagesCount ?? book.pagesCount,
       editionYear: editionYear ?? book.editionYear,
@@ -112,7 +120,7 @@ export default class BooksController {
       return response.forbidden({ message: 'You do not have permission to delete this book.' })
     }
 
-    await book.delete()
-    return response.noContent()
+    await book.delete() // supprimer le livre
+    return response.noContent() // renvoie le code 204 ( No Content )
   }
 }
